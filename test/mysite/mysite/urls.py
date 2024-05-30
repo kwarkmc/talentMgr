01bbfc9591d1e802a87ef42b737aa4aa59091b7a
Kwark <kwarkmc99@gmail.com>
2024-05-29 23:21:05 -0700

    Add TEST Project for staging

--- test/mysite/mysite/urls.py
+++ test/mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for talentMgr project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-	path('TEST/', include('TEST.urls')),
     path('admin/', admin.site.urls),
 ]
